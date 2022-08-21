c678d7b0890e82ef14e33f02ec3e9dd1ba66281a
Tahsin2020 <62449141+Tahsin2020@users.noreply.github.com>
2022-08-21 00:13:04 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,13 +13,6 @@ import './App.css';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAYIZUWotMiaPxuzG3t1iHIVFHak7eOPgc",
-  authDomain: "consistency-81cb2.firebaseapp.com",
-  projectId: "consistency-81cb2",
-  storageBucket: "consistency-81cb2.appspot.com",
-  messagingSenderId: "325992491740",
-  appId: "1:325992491740:web:7b473237b6a16ccda8e219",
-  measurementId: "G-GWNQFG2GEF"
 };
 
 // Initialize Firebase
